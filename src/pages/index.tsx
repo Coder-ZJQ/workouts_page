b2583c0de4d77017651fcc4b9fce1a6c3e345915
zhaojq <zjq_joker@163.com>
2024-01-06 16:36:13 +0800

    feat: running -> workout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -51,7 +51,7 @@ const Index = () => {
     scrollToMap();
     setActivity(filterAndSortRuns(activities, item, func, sortDateFunc));
     setRunIndex(-1);
-    setTitle(`${item} ${name} Running Heatmap`);
+    setTitle(`${item} ${name} Workout Heatmap`);
   };
 
   const changeYear = (y: string) => {
